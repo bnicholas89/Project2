c404ab2097e9dfa6bb5256e8a822dcd214030426
Nicholas Bates <nbates66@gmail.com>
2020-02-08 07:56:23 -0800

    defined intro, choices, and print_pause functions.

---
+++ adventure_game.py
@@ -0,0 +1,39 @@
+import time
+
+
+def print_pause(message_to_print):
+    print(message_to_print)
+    time.sleep(2)
+
+
+def house():
+    # Things that happen to the player in the house
+
+def cave():
+    # Things that happen to the player in the cave
+
+def field():
+    # Things that happen to the player in the cave
+
+
+def intro():
+    print_pause("You find yourself in a dark dungeon.")
+    print_pause("In front of you are two passageways.")
+
+
+def choices():
+    print_pause("Enter 1 to knock on the door of the house.")
+    print_pause("Enter 2 to peer into the cave.")
+    choice = input("1. Knock on the door of the house."
+                   "2. Peer into the cave.")
+
+
+
+
+intro()
+
+
+choices()
+
+
+
