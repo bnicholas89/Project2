a832f8fa1c4abdb682ff3c320891c0ce6eb90761
Nicholas Bates <nbates66@gmail.com>
2020-02-09 13:19:55 -0800

    more flavor text, added variable for mymonster where needed to work with 1 argument print_pause function. Defined house().

--- adventure_game.py
+++ adventure_game.py
@@ -6,16 +6,16 @@ monsters = ["Gorgon", "Kikimora", "Bruxa"]
 mymonster = random.choice(monsters)
 
 
-def house(choice):
-# Things that happen to the player in the cave
+def house():
+    print_pause("You approach the door of the house.")
+    print_pause("You are about to knock when the door opens and out steps a " +mymonster)
+    print_pause("Eep! This is the " +mymonster+ "'s house!")
+    print_pause("The " +mymonster+ " attacks you!")
+    print_pause("You feel a bit under-prepared for this, what with only have a tiny dagger.")
 
 
-def cave(choice):
-# Things that happen to the player in the cave
-
-
-def field(choice):
-# Things that happen to the player in the cave
+def cave():
+    print_pause()
 
 
 def print_pause(message_to_print):
@@ -25,7 +25,8 @@ def print_pause(message_to_print):
 
 def intro():
     print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause("Rumor has it that a",random.choice(monsters), "is somewhere around here, and has been terrifying the nearby village.")
+    print_pause("Rumor has it that a " +mymonster+ "is somewhere around here, and has been terrifying the nearby "
+                                                   "village.")
     print_pause("In front of you is a house.")
     print_pause("To your right is a dark cave.")
     print_pause("In your hand you hold your trusty (but not very effective) dagger.")
@@ -38,13 +39,13 @@ def choices():
     print_pause("(Please enter 1 or 2.)")
     choice = input("1. Knock on the door of the house."
                    "2. Peer into the cave.")
-
-    if choice == '1':
-        print_pause("You approach the door of the house.")
-        print_pause("You are about to knock when the door opens and out steps a", mymonster)
-        print_pause("Eep! This is the troll's house!")
-        print_pause("The", mymonster, "attacks you!")
-        print_pause("You feel a bit under-prepared for this, what with only have a tiny dagger.")
+    while False:
+        if choice == '1':
+            house()
+        if choice == '2':
+            cave()
+        else:
+            print_pause("(Please enter 1 or 2.)")
 
 
 def play_game():
@@ -54,6 +55,3 @@ def play_game():
 
 
 play_game()
-
-
-
