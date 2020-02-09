18cc70f08498ff46449aab6cdd94a72ca9431345
Nicholas Bates <nbates66@gmail.com>
2020-02-08 16:49:42 -0800

    more flavor text, random choice for monster and variable to store the random monster to persist through game.

--- adventure_game.py
+++ adventure_game.py
@@ -1,8 +1,21 @@
 import time
 import random
 
+monsters = ["Gorgon", "Kikimora", "Bruxa"]
 
-items []
+mymonster = random.choice(monsters)
+
+
+def house(choice):
+# Things that happen to the player in the cave
+
+
+def cave(choice):
+# Things that happen to the player in the cave
+
+
+def field(choice):
+# Things that happen to the player in the cave
 
 
 def print_pause(message_to_print):
@@ -10,24 +23,12 @@ def print_pause(message_to_print):
     time.sleep(2)
 
 
-def house(choice):
-     # Things that happen to the player in the cave
-
-
-def cave(choice):
-    # Things that happen to the player in the cave
-
-def field(choice):
-    # Things that happen to the player in the cave
-
-
 def intro():
     print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause("Rumor has it that a #random monster# is somewhere around here, and has been terrifying the nearby village.")
+    print_pause("Rumor has it that a",random.choice(monsters), "is somewhere around here, and has been terrifying the nearby village.")
     print_pause("In front of you is a house.")
     print_pause("To your right is a dark cave.")
     print_pause("In your hand you hold your trusty (but not very effective) dagger.")
-
 
 
 def choices():
@@ -40,9 +41,10 @@ def choices():
 
     if choice == '1':
         print_pause("You approach the door of the house.")
-        print_pause("You are about to knock when the door opens and out steps a #random monster#")
-
-
+        print_pause("You are about to knock when the door opens and out steps a", mymonster)
+        print_pause("Eep! This is the troll's house!")
+        print_pause("The", mymonster, "attacks you!")
+        print_pause("You feel a bit under-prepared for this, what with only have a tiny dagger.")
 
 
 def play_game():
