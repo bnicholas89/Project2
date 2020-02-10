1f7bde6e4b33b487fa1a17ae8d18a25a6447eb35
Nicholas Bates <nbates66@gmail.com>
2020-02-09 17:04:29 -0800

    rearranged code for readability.

--- adventure_game.py
+++ adventure_game.py
@@ -6,18 +6,6 @@ monsters = ["Gorgon", "Kikimora", "Bruxa"]
 mymonster = random.choice(monsters)
 
 
-def house():
-    print_pause("You approach the door of the house.")
-    print_pause("You are about to knock when the door opens and out steps a " +mymonster)
-    print_pause("Eep! This is the " +mymonster+ "'s house!")
-    print_pause("The " +mymonster+ " attacks you!")
-    print_pause("You feel a bit under-prepared for this, what with only have a tiny dagger.")
-
-
-def cave():
-    print_pause()
-
-
 def print_pause(message_to_print):
     print(message_to_print)
     time.sleep(2)
@@ -25,11 +13,25 @@ def print_pause(message_to_print):
 
 def intro():
     print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause("Rumor has it that a " +mymonster+ "is somewhere around here, and has been terrifying the nearby "
-                                                   "village.")
+    print_pause("Rumor has it that a " + mymonster + "is somewhere around here, and has been terrifying the nearby "
+                                                     "village.")
     print_pause("In front of you is a house.")
     print_pause("To your right is a dark cave.")
     print_pause("In your hand you hold your trusty (but not very effective) dagger.")
+
+
+def house():
+    print_pause("You approach the door of the house.")
+    print_pause("You are about to knock when the door opens and out steps a " + mymonster)
+    print_pause("Eep! This is the " + mymonster + "'s house!")
+    print_pause("The " + mymonster + " attacks you!")
+    print_pause("You feel a bit under-prepared for this, what with only have a tiny dagger.")
+    print_pause("Would you like to (1) fight or (2) run away?")
+    choices()
+
+
+def cave():
+    print_pause()
 
 
 def choices():
@@ -49,8 +51,8 @@ def choices():
 
 
 def play_game():
-    items = []
     intro()
+    house()
     choices()
 
 
