e0c9f6c546195803624d45748af29b1885d003d3
Nicholas Bates <nbates66@gmail.com>
2020-02-26 19:30:29 -0800

    rearranged code for readability.

--- adventure_game.py
+++ adventure_game.py
@@ -18,6 +18,7 @@ def intro():
     print_pause("In front of you is a house.")
     print_pause("To your right is a dark cave.")
     print_pause("In your hand you hold your trusty (but not very effective) dagger.")
+    print_pause()
 
 
 def house():
