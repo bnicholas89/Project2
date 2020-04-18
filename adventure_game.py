5d8c22468e792dfbac1c5c33938443067bb45d10
Nicholas Bates <nbates66@gmail.com>
2020-04-17 19:46:38 -0700

    rearranged code for readability.

--- adventure_game.py
+++ adventure_game.py
@@ -13,7 +13,7 @@ def print_pause(message_to_print):
 
 def intro():
     print_pause("You find yourself standing in an open field, filled with grass and yellow wildflowers.")
-    print_pause("Rumor has it that a " + mymonster + "is somewhere around here, and has been terrifying the nearby "
+    print_pause("Rumor has it that a " + mymonster + " is somewhere around here, and has been terrifying the nearby "
                                                      "village.")
     print_pause("In front of you is a house.")
     print_pause("To your right is a dark cave.")
